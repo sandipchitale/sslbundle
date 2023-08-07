d2340367eb6671f26e188bb19b5ec006c78c60d2
SANDIP CHITALE <sandipchitale@gmail.com>
2023-08-06 18:33:54 -0700

    Support printCertificates profile.

--- src/main/java/sandipchitale/jdk/sslbundle/SslbundleApplication.java
+++ src/main/java/sandipchitale/jdk/sslbundle/SslbundleApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.ssl.SslBundle;
 import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.client.RestTemplate;
 
 import java.security.KeyStore;
@@ -14,24 +17,32 @@ import java.security.KeyStore;
 @SpringBootApplication
 public class SslbundleApplication {
 
+	@Profile("printCertificates")
 	@Bean
-	public CommandLineRunner jdkSslBundle(SslBundles sslBundles, RestTemplateBuilder restTemplateBuilder) {
-	    return (args) -> {
+	public CommandLineRunner printCertificates(SslBundles sslBundles, RestTemplateBuilder restTemplateBuilder) {
+		return (args) -> {
 			SslBundle sslBundle = sslBundles.getBundle(JDKSslBundleRegistrar.JAVA_SSL_BUNDLE);
 			if (sslBundle != null) {
-	//			KeyStore trustStore = sslBundle.getStores().getTrustStore();
-	//			trustStore.aliases().asIterator().forEachRemaining(alias -> {
-	//				try {
-	//					System.out.println(
-	//							"\n--------------------------------------------------------------------" +
-	//							"\nAlias: " + alias +
-	//							"\nCertificate: " + trustStore.getCertificate(alias));
-	//				} catch (Exception ignore) {
-	//				}
-	//			});
+				KeyStore trustStore = sslBundle.getStores().getTrustStore();
+				trustStore.aliases().asIterator().forEachRemaining(alias -> {
+					try {
+						System.out.println(
+								"\n--------------------------------------------------------------------" +
+								"\nAlias: " + alias +
+								"\nCertificate: " + trustStore.getCertificate(alias));
+					} catch (Exception ignore) {
+					}
+				});
+			}
+		};
+	}
 
+	@Bean
+	public CommandLineRunner postmanEchoGet(SslBundles sslBundles, RestTemplateBuilder restTemplateBuilder) {
+		return (args) -> {
+			SslBundle sslBundle = sslBundles.getBundle(JDKSslBundleRegistrar.JAVA_SSL_BUNDLE);
+			if (sslBundle != null) {
 				RestTemplate restTemplate = restTemplateBuilder.setSslBundle(sslBundle).build();
-	//			RestTemplate restTemplate = restTemplateBuilder.build();
 				System.out.println(restTemplate.getForEntity("https://postman-echo.com/get", String.class).getBody());
 			}
 		};
